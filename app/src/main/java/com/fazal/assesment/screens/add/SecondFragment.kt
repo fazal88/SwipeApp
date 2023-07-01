b399085482d7717b273efc7fec380185148a205e
Fazal <fazalsultan88@gmail.com>
2023-07-02 03:40:23 +0530

    added fields and validation detail save screen

--- app/src/main/java/com/fazal/assesment/screens/add/SecondFragment.kt
+++ app/src/main/java/com/fazal/assesment/screens/add/SecondFragment.kt
@@ -1,12 +1,14 @@
 package com.fazal.assesment.screens.add
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.fazal.assesment.databinding.FragmentSecondBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
 /**
  * Created by Fazal on 01/07/23.
  * Copyright (c) 2023 Mohd Fazal Shaikh. All rights reserved.
@@ -16,11 +18,12 @@ import com.fazal.assesment.databinding.FragmentSecondBinding
  */
 class SecondFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
+    private var _binding: FragmentSecondBinding? = null
     private val binding get() = _binding!!
+    private val vm by viewModel<SecondViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,8 +37,50 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(SecondFragmentDirections.actionSecondFragmentToFirstFragment())
+
+        _binding?.vm = vm
+        _binding?.lifecycleOwner = viewLifecycleOwner
+
+        vm.navigateToList.observe(viewLifecycleOwner) {
+            it?.let {
+                if (it) {
+                    findNavController().navigate(SecondFragmentDirections.actionSecondFragmentToFirstFragment())
+                    vm.reset()
+                }
+            }
+        }
+        vm.name.observe(viewLifecycleOwner) {
+            binding.name.isErrorEnabled = it.isEmpty()
+        }
+        vm.type.observe(viewLifecycleOwner) {
+            binding.type.isErrorEnabled = it.isEmpty()
+        }
+        vm.tax.observe(viewLifecycleOwner) {
+            binding.tax.isErrorEnabled = it.isEmpty()||(it.toInt() in 1..99).not()
+        }
+        vm.price.observe(viewLifecycleOwner) {
+            binding.price.isErrorEnabled = it.isEmpty()||it.toDouble() <= 0
+        }
+
+
+        vm.nameError.observe(viewLifecycleOwner) {
+            binding.name.isErrorEnabled = it.isNotEmpty()
+            if (it.isNotEmpty()) binding.name.error = it
+        }
+
+        vm.typeError.observe(viewLifecycleOwner) {
+            binding.type.isErrorEnabled = it.isNotEmpty()
+            if (it.isNotEmpty()) binding.type.error = it
+        }
+
+        vm.taxError.observe(viewLifecycleOwner) {
+            binding.tax.isErrorEnabled = it.isNotEmpty()
+            if (it.isNotEmpty()) binding.tax.error = it
+        }
+
+        vm.priceError.observe(viewLifecycleOwner) {
+            binding.price.isErrorEnabled = it.isNotEmpty()
+            if (it.isNotEmpty()) binding.price.error = it
         }
     }
 
--- app/src/main/java/com/fazal/assesment/screens/add/SecondViewModel.kt
+++ app/src/main/java/com/fazal/assesment/screens/add/SecondViewModel.kt
@@ -1,9 +1,9 @@
 package com.fazal.assesment.screens.add
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.fazal.assesment.api.SwipeRepository
 import com.fazal.assesment.api.SwipeRepositoryImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,12 +16,71 @@ class SecondViewModel(
     private val repo : SwipeRepositoryImpl
 ) : ViewModel() {
 
-    val list = repo.listProducts
+    private var _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean>
+        get() = _loading
 
-    init {
-        viewModelScope.launch(Dispatchers.IO) {
-            repo.getProducts()
+    private val _error = MutableLiveData<String>("")
+    val error: LiveData<String>
+        get() = _error
+
+    private var _navigateToList = MutableLiveData<Boolean>()
+    val navigateToList: LiveData<Boolean>
+        get() = _navigateToList
+    fun goToList(){
+        _navigateToList.postValue(true)
+    }
+
+    val name = MutableLiveData<String>("")
+    val nameError = MutableLiveData<String>("")
+    val type = MutableLiveData<String>("")
+    val typeError = MutableLiveData<String>("")
+    val tax = MutableLiveData<String>("")
+    val taxError= MutableLiveData<String>("")
+    val price = MutableLiveData<String>("")
+    val priceError = MutableLiveData<String>("")
+
+    val saveResponse = repo.saveResponse
+
+    fun save(){
+        if(valid()){
+            //save
+            goToList()
         }
+    }
+
+    private fun valid(): Boolean {
+        var isValid = true
+        if(name.value.isNullOrEmpty()){
+            isValid = false
+            nameError.postValue("Product Name cannot be empty")
+        }
+        if(type.value.isNullOrEmpty()){
+            isValid = false
+            typeError.postValue("Product Type cannot be empty")
+        }
+        if(tax.value.isNullOrEmpty()){
+            isValid = false
+            taxError.postValue("tax value cannot be empty")
+        }else if((tax.value?.toInt() in 1..99).not()){
+            isValid = false
+            taxError.postValue("tax value is unacceptable")
+        }
+        if(price.value.isNullOrEmpty()){
+            isValid = false
+            priceError.postValue("Selling price cannot be empty")
+        }else if(price.value?.toDouble()!! <= 0){
+            isValid = false
+            priceError.postValue("Selling price is unacceptable")
+        }
+        return isValid
+    }
+
+
+    fun reset() {
+        _navigateToList.postValue(false)
+        _loading.postValue(false)
+        _error.postValue("")
     }
 
 }
